705db0c1d80ef124e7a557077c48a15bfed95f3e
Jeff Tarimo <tarimojeff@gmail.com>
2026-03-04 14:05:00 +0300

    changed issue with device control

--- devices/urls.py
+++ devices/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # Device list and details
     path("", views.device_list, name="device_list"),
+    path("change_status/", views.change_device_status, name="change_device_status"),
     path("change_status_partial/", views.change_device_status_partial, name="change_device_status_partial"),
     path("schedules/", views.DeviceScheduleListView.as_view(), name="device_schedule_list"),
     path("schedules/add/", views.DeviceScheduleCreateView.as_view(), name="device_schedule_add"),
@@ -12,7 +13,6 @@ urlpatterns = [
     path("schedules/<int:pk>/trigger/", views.trigger_schedule, name="device_schedule_trigger"),
     path("create/", views.device_create, name="device_create"),
     path("<str:deviceid>/", views.device_detail, name="device_detail"),
-    path("change_status/", views.change_device_status, name="change_device_status"),
     path("edit/<str:deviceid>/", views.device_edit, name="device_edit"),
     path("delete/<str:deviceid>/", views.device_delete, name="device_delete"),
     path("live/<str:deviceid>/", views.device_live_view, name="device_live"),
