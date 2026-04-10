3abbc5031bd0591b54bf6e72d14c4c3e41a96407
Jeff Tarimo <tarimojeff@gmail.com>
2026-04-10 13:36:28 +0300

    api changes due to namespace error

--- api/urls.py
+++ api/urls.py
@@ -15,7 +15,7 @@ app_name = "api"
 
 urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="api:schema"), name="swagger-ui"),
 
     # Devices Info: Only detail view
     
